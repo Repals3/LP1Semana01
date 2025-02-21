4c69f6080fe0cc25eb622dac36640283b46e90d2
Diogo Coelho <diogocoelho.dev@gmail.com>
2025-02-21 13:57:55 +0000

    Valores Inteiros

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero = 3;
+            Console.WriteLine(numero);
+        }
+    }
+}
