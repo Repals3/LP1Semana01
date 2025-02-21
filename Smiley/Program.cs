c32b5b168cf008d26a5ec04f25e5e08e620a4918
Diogo Coelho <diogocoelho.dev@gmail.com>
2025-02-21 18:02:04 +0000

    Avaliação aula 1

--- Smiley/Program.cs
+++ Smiley/Program.cs
@@ -11,12 +11,32 @@ namespace Smiley
             // inserir código 1
             Console.WriteLine("Insere código: ");
             //inserir código
-            string code= Console.ReadLine();
+            string code1= Console.ReadLine();
             //converter para int
-            int x= int.Parse(code,NumberStyles.AllowHexSpecifier);
+            int x1= int.Parse(code1,NumberStyles.AllowHexSpecifier);
             // escrever resultado
             Console.OutputEncoding= Encoding.UTF8;
-            Console.WriteLine("Carácter Unicode:" + x);
+            Console.WriteLine("Carácter Unicode:" + x1);
+
+              // inserir código 2
+            Console.WriteLine("Insere código: ");
+            //inserir código
+            string code2= Console.ReadLine();
+            //converter para int
+            int x2= int.Parse(code2,NumberStyles.AllowHexSpecifier);
+            // escrever resultado
+            Console.OutputEncoding= Encoding.UTF8;
+            Console.WriteLine("Carácter Unicode:" + x2);
+
+              // inserir código 3
+            Console.WriteLine("Insere código: ");
+            //inserir código
+            string code3= Console.ReadLine();
+            //converter para int
+            int x3= int.Parse(code3,NumberStyles.AllowHexSpecifier);
+            // escrever resultado
+            Console.OutputEncoding= Encoding.UTF8;
+            Console.WriteLine("Carácter Unicode:" + x3);
         }
     }
 }
