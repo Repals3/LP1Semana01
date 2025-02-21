7962530d299ce73be524df83fbeb127eef67d1ac
Diogo Coelho <diogocoelho.dev@gmail.com>
2025-02-21 17:58:52 +0000

    Avaliação aula

---
+++ Smiley/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // inserir código 1
+            Console.WriteLine("Insere código: ");
+            //inserir código
+            string code= Console.ReadLine();
+            //converter para int
+            int x= int.Parse(code,NumberStyles.AllowHexSpecifier);
+            // escrever resultado
+            Console.OutputEncoding= Encoding.UTF8;
+            Console.WriteLine("Carácter Unicode:" + x);
+        }
+    }
+}
